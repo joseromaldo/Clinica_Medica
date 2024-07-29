09520670486c109b5e251f6f8581d1b431d08788
Maldonado <joseromaldo.1997@gmail.com>
2024-07-29 07:44:09 -0600

    Creación de includes

---
+++ includes/navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPrincipal" aria-controls="navbarPrincipal" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarPrincipal">
+            <a class="navbar-brand" href="/Clinica_Medica/views/home/index.php">Clínica Médica</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/Clinica_Medica/views/home/index.php">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Manto.
+                    </a>
+                    <ul class="dropdown-menu bg-primary">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/formaD_conJS/views/grado/index.php">Paciente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/formaD_conJS/views/grado/index.php">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/formaD_conJS/views/grado/index.php">Detalle</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
